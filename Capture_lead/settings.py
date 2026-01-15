bb173e771f67bcf9f7ad439bc6d374e77c7df6b3
mathRyan889 <matheusryan889@gmail.com>
2026-01-15 06:27:27 -0300

    Update settings

--- Capture_lead/settings.py
+++ Capture_lead/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#h$fyy&!xi=_$1lyg6ofwh2zaf@ogu5jlg&i4t1dwc_)g2gt0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['multiplacreditos7.pythonanywhere.com']
 
 
 # Application definition
@@ -119,4 +120,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
