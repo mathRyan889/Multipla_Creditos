7dba2d8402f5fa911645d5e41d5236fa8565221e
mathRyan889 <matheusryan889@gmail.com>
2026-01-15 23:41:43 -0300

    update views

--- register_lead/views.py
+++ register_lead/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterLeadForm
-from .models import Service  # Importe o modelo de Serviço
 
 def home(request):
     if request.method == 'POST':
         form = RegisterLeadForm(request.POST)
         if form.is_valid():
             form.save()
-            # É melhor usar redirect aqui para "limpar" o formulário após o envio
+            # O redirect é essencial para limpar o form e recarregar a lista do banco
             return redirect('home') 
     else:
-        # O Django usará o queryset definido no seu forms.py ou no model
         form = RegisterLeadForm()
     
-    # IMPORTANTE: O formulário DEVE ser passado no contexto
     return render(request, 'home.html', {'form': form})
-
-def about(request):
-    return render(request, 'about.html')
