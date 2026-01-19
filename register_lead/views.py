93dc75d41797fe23323e2cb1678ec98ba20c0fd1
mathRyan889 <matheusryan889@gmail.com>
2026-01-19 13:19:18 -0300

    notificação de leads novos

--- register_lead/views.py
+++ register_lead/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+import requests
+import json
 from .forms import RegisterLeadForm
 
 def home(request):
@@ -17,3 +19,22 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, 'about.html')
+
+
+
+def send_pushbullet_notification(lead):
+    access_token = "o.StnBdNNyn3FGPDhDTB3sHBgBp4QJZRE6"
+    url = "https://api.pushbullet.com/v2/pushes"
+    
+    payload = {
+        "type": "note",
+        "title": "🔥 Novo Lead Capturado",
+        "body": f"Nome: {lead.name}\nServiço: {lead.services.type_service}\nWhats: {lead.whatsapp}"
+    }
+    
+    headers = {
+        "Access-Token": access_token,
+        "Content-Type": "application/json"
+    }
+    
+    requests.post(url, data=json.dumps(payload), headers=headers)
